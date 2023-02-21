575f3232f2731954ac0d0ccf9151030ab6d3d6fc
Hubchicken <85572294+Exruw@users.noreply.github.com>
2023-02-21 15:35:05 -0500

    Update schizoarray.js

--- schizoarray.js
+++ schizoarray.js
@@ -1,37 +1,39 @@
-const schizoimages = [];
 const tensions = {"png": true};
-(await (await fetch(`https://api.github.com/repos/Exruw/schizoimages/git/trees/${(await (await fetch("https://api.github.com/repos/Exruw/hubvideos/commits")).json())[0]?.commit?.tree?.sha}`)).json()).tree.forEach(v => {
-	if (v?.type !== "tree" && v?.path && tensions[v.path.split(".").pop()])
-    schizoimages.push(`https://schizo.hubchicken.tk/${encodeURIComponent(v.path)}`);
+const schizoimages = [];
+
+(await (await fetch(`https://api.github.com/repos/Exruw/schizoimages/git/trees/main`)).json()).tree.forEach(v => {
+    if (v?.type !== "tree" && v?.path && tensions[v.path.split(".").pop()])
+        schizoimages.push(`https://schizo.hubchicken.tk/${encodeURIComponent(v.path)}`);
 });
-export function schizojump()
-{
-	return schizoimages[0|(Math.random() * schizoimages.length)]
+
+export function schizojump() {
+    return schizoimages[0|(Math.random() * schizoimages.length)]
 }
 
-			const sg = document.createElement('div');
-			sg.style.position = 'absolute';
-			sg.style.top = '0';
-			sg.style.left = '0';
-			sg.style.width = '100%';
-			sg.style.height = '100%';
-			document.body.appendChild(sg);
+const sg = document.createElement('div');
+sg.style.position = 'absolute';
+sg.style.top = '0';
+sg.style.left = '0';
+sg.style.width = '100%';
+sg.style.height = '100%';
+document.body.appendChild(sg);
 
+export function createJuump() {
+    const juump = document.createElement('div');
+    juump.id = 'juump';
+    juump.style.backgroundImage = `url(${schizojump()})`;
+    sg.appendChild(juump);
 
-			export function createJuump() {
-				const juump = document.createElement('div');
-				juump.id = 'juump';
-				juump.style.backgroundImage = `url(${schizoimages[Math.floor(Math.random() * (max - min + 1)) + min(0, schizoimages.length - 1)]})`;
-				sg.appendChild(juump);
-
-				const speed = Math.floor(Math.random() * (max - min + 1)) + min(45, 110);
-				let i = 0;
-				const intervalId = setInterval(() => {
-					i++;
-					juump.style.opacity = i / speed;
-					if (i === speed) {
-						clearInterval(intervalId);
-						sg.removeChild(juump);
-					}
-				}, 10);
-			}
+    const min = 0;
+    const max = schizoimages.length - 1;
+    const speed = Math.floor(Math.random() * (max - min + 1)) + min(45, 110);
+    let i = 0;
+    const intervalId = setInterval(() => {
+        i++;
+        juump.style.opacity = i / speed;
+        if (i === speed) {
+            clearInterval(intervalId);
+            sg.removeChild(juump);
+        }
+    }, 10);
+}
